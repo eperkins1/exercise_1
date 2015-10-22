1ad608454352dd8ec7b59d55d484db0e6e9a39b9
eperkins1 <eperkins27@gmail.com>
2015-10-22 14:36:56 -0700

    added join clause to hos_procedure, so this is second attempt

--- transforming/hos_procedure_join.sql
+++ transforming/hos_procedure_join.sql
@@ -2,5 +2,6 @@
 DROP TABLE hos_procedure;
 CREATE TABLE hos_procedure AS
 SELECT a.*, b.overall_achieve, b.hcahps_base_score, b.hcahps_consistency_score
-FROM hospitals a, survey_results b
+FROM hospitals a
+LEFT OUTER JOIN survey_results b
 ON (a.provider_id = b.provider_id);
