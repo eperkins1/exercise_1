09c439520918fb3e3897c9fc3294976620cf3461
eperkins1 <eperkins27@gmail.com>
2015-10-21 20:30:40 -0700

    Added folder for transforming tables

---
+++ transforming/complications_hospital_reduce.sql
@@ -0,0 +1,5 @@
+#Transformations plan
+CREATE TABLE complic_hos_reduce AS
+SELECT provider_id, hospital_name, state, measure_name, measure_id, compared_to_national, denominator, score FROM complic_hos;
+
+
