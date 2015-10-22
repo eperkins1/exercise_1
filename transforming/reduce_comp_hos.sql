dc7448bc75c2d4a3c5598e455e9f4e42e2ca2afe
eperkins1 <eperkins27@gmail.com>
2015-10-21 21:06:28 -0700

    changed

--- transforming/reduce_comp_hos.sql
+++ transforming/reduce_comp_hos.sql
@@ -1,4 +1,3 @@
-/*Transformations plan*/
 CREATE TABLE complic_hos_reduce AS
 SELECT provider_id, hospital_name, state, measure_name, measure_id, compared_to_national, denominator, score FROM complic_hos;
 
