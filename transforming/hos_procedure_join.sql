f6b4605c489112c48cac0b861d0f7f0a9ae0c275
eperkins1 <eperkins27@gmail.com>
2015-10-22 14:25:50 -0700

    Added hos_procedure_join.sql first attempt

---
+++ transforming/hos_procedure_join.sql
@@ -0,0 +1,6 @@
+--Build hospital_procedure table--
+DROP TABLE hos_procedure;
+CREATE TABLE hos_procedure AS
+SELECT a.*, b.overall_achieve, b.hcahps_base_score, b.hcahps_consistency_score
+FROM hospitals a, survey_results b
+ON (a.provider_id = b.provider_id);
