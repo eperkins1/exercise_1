3c0534efd6f5b840d1ea5bc932153dc35fd452ae
eperkins1 <eperkins27@gmail.com>
2015-10-22 15:02:25 -0700

    Modified hos_procedure_join's name, and made first pass at procedures table

--- transforming/hos_procedure_join.sql
+++ transforming/hos_procedure_join.sql
@@ -1,6 +1,6 @@
 --Build hospital_procedure table--
-DROP TABLE hos_procedure;
-CREATE TABLE hos_procedure AS
+DROP TABLE hos_survey;
+CREATE TABLE hos_survey AS
 SELECT a.*, b.overall_achieve, b.hcahps_base_score, b.hcahps_consistency_score
 FROM hospitals a
 LEFT OUTER JOIN survey_results b
---
+++ transforming/procedures_union.sql
@@ -0,0 +1,11 @@
+--Build procedures table from measure_dates and timely_national union-- 
+DROP TABLE procedures;
+CREATE TABLE procedures AS
+SELECT DISTINCT measure_id
+FROM (
+    SELECT DISTINCT measure_id
+    FROM timely_national
+    UNION ALL
+    SELECT DISTINCT measure_id
+    FROM measure_dates
+ );
