d33e1e1409a68ee944439b6abde4f247eedd9f18
eperkins1 <eperkins27@gmail.com>
2015-10-22 15:34:24 -0700

    Procedures_with_dupes now working, this is first try to make procedures with 128 distinct rows of only measure ids

--- transforming/procedures_union.sql
+++ transforming/procedures_union.sql
@@ -1,8 +1,15 @@
---Build procedures table from measure_dates and timely_national union-- 
-DROP TABLE procedures;
-CREATE TABLE procedures AS
+--Build procedures_with_dupes table from measure_dates and timely_national union-- 
+DROP TABLE procedures_with_dupes;
+CREATE TABLE procedures_with_dupes AS
 SELECT DISTINCT measure_id
 FROM timely_national
 UNION ALL
 SELECT DISTINCT measure_id
 FROM measure_dates;
+
+--Now create table with unique procedure IDs--
+DROP TABLE procedures;
+CREATE TABLE procedures AS
+SELECT DISTINCT measure_id
+FROM procedures_with_dupes;
+DROP TABLE procedures_with_dupes;
