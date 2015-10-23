4f96598d36c229227caf76707b2aa3ee9c471d72
eperkins1 <eperkins27@gmail.com>
2015-10-22 17:10:30 -0700

    First try at state-procedures union

---
+++ transforming/state_procedures_union.sql
@@ -0,0 +1,19 @@
+--Build state_procedures table from complic_state, readmis_state, and timely_state--
+
+--First step: Union complic and readmis--
+DROP TABLE complic_readmis;
+CREATE TABLE complic_readmis AS
+SELECT state, measure_id, measure_name, num_hospitals_better, num_hospitals_worse
+FROM complic_state
+UNION ALL
+SELECT state, measure_id, measure_name, num_hospitals_better, num_hospitals_worse
+FROM readmis_state;
+
+--Second step: Union timely with complic_readmis--
+DROP TABLE state_procedures;
+CREATE TABLE state_procedures AS
+SELECT state, measure_id, measure_name, num_hospitals_better, num_hospitals_worse, Null as score
+FROM complic_readmis
+UNION
+SELECT state, measure_id, measure_name, Null as num_hospitals_better, Null as num_hospitals_worse, score
+FROM timely_state;
