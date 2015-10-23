3a5cec5806f64cca8e082aa6a94e79b2d0283dfa
eperkins1 <eperkins27@gmail.com>
2015-10-23 16:30:35 -0700

    Updated ERD with important hos_procedures table

--- transforming/state_procedures_union.sql
+++ transforming/state_procedures_union.sql
@@ -1,8 +1,8 @@
 --Build state_procedures table from complic_state, readmis_state, and timely_state--
 
 --First step: Union complic and readmis--
-DROP TABLE complic_readmis;
-CREATE TABLE complic_readmis AS
+DROP TABLE complic_readmis_state;
+CREATE TABLE complic_readmis_state AS
 SELECT state, measure_id, measure_name, num_hospitals_better, num_hospitals_worse
 FROM complic_state
 UNION ALL
@@ -13,7 +13,7 @@ FROM readmis_state;
 DROP TABLE state_procedures;
 CREATE TABLE state_procedures AS
 SELECT state, measure_id, measure_name, num_hospitals_better, num_hospitals_worse, Null as score
-FROM complic_readmis
+FROM complic_readmis_state
 UNION ALL
 SELECT state, measure_id, measure_name, Null as num_hospitals_better, Null as num_hospitals_worse, score
 FROM timely_state;
